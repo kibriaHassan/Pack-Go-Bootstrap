e0ec8d4668a2f14c53c8751b989b490f2746a408
kibriaHassan <kibriahassan3500@gmail.com>
2025-11-24 15:29:12 +0600

    updates now

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ $('.responsive').slick({
   dots: true,
   infinite: true,
   speed: 300,
-  slidesToShow: 5,
+  slidesToShow: 4,
   slidesToScroll: 4,
+  centerMode: true,      
+  centerPadding: '100px', 
   responsive: [
     {
       breakpoint: 1024,
@@ -18,14 +20,18 @@ $('.responsive').slick({
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        centerMode: true,
+        centerPadding: '40px'
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '30px'
       }
     }
   ]
